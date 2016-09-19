3948daf1ee8789b34849b54547090f042e8645d7
rg <uuuzombie@gmail.com>
2016-09-19 23:14:10 +0800

    add common datasource cache

---
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_db/cache/CommonDataSourceCache.java
@@ -0,0 +1,12 @@
+package com.sky.demo.web_demo_multi_tenant_separate_db.cache;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by rg on 9/19/16.
+ */
+@Service
+public class CommonDataSourceCache {
+
+
+}
