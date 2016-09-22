23ded48ab073cf39e8be19b60a5d0dcf5ecd8392
rg <uuuzombie@gmail.com>
2016-09-22 13:59:12 +0800

    add get JdbcTemplate method

--- src/main/java/com/sky/demo/web_demo_multi_tenant_separate_db/controller/CommonDataSourceCacheController.java
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_db/controller/CommonDataSourceCacheController.java
@@ -8,8 +8,10 @@ import com.sky.demo.web_demo_multi_tenant_separate_db.util.RetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -43,6 +45,58 @@ public class CommonDataSourceCacheController {
             result = RetUtil.buildSuccessRet(urlMap);
         } catch (Exception e) {
             logger.error("query all jdbcTemplate error", e);
+            result = RetUtil.buildErrorRet(RetStatus.QUERY_ERROR);
+        }
+        return result;
+    }
+
+    @RequestMapping("/getAllNamedParameterJdbcTemplate")
+    @ResponseBody
+    public RetData<Map<String, String>> getAllNamedParameterJdbcTemplate() {
+        RetData<Map<String, String>> result = null;
+
+        try {
+            Map<String, NamedParameterJdbcTemplate> allNamedParameterJdbcTemplate = commonDataSourceCache.getAllNamedParameterJdbcTemplate();
+
+            Map<String, String> urlMap = Maps.newHashMap();
+            for (Map.Entry<String, NamedParameterJdbcTemplate> entry : allNamedParameterJdbcTemplate.entrySet()) {
+                urlMap.put(entry.getKey(), entry.getValue().getJdbcOperations().toString());
+            }
+            result = RetUtil.buildSuccessRet(urlMap);
+        } catch (Exception e) {
+            logger.error("query all namedParameterJdbcTemplate error", e);
+            result = RetUtil.buildErrorRet(RetStatus.QUERY_ERROR);
+        }
+        return result;
+    }
+
+    @RequestMapping("/getJdbcTemplate")
+    @ResponseBody
+    public RetData<String> getJdbcTemplate(@RequestParam String tenant) {
+        RetData<String> result = null;
+
+        try {
+            JdbcTemplate jdbcTemplate = commonDataSourceCache.getJdbcTemplate(tenant);
+
+            result = RetUtil.buildSuccessRet(jdbcTemplate.getDataSource().getConnection().getMetaData().getURL());
+        } catch (Exception e) {
+            logger.error("query jdbcTemplate error", e);
+            result = RetUtil.buildErrorRet(RetStatus.QUERY_ERROR);
+        }
+        return result;
+    }
+
+    @RequestMapping("/getNamedParameterJdbcTemplate")
+    @ResponseBody
+    public RetData<String> getNamedParameterJdbcTemplate(@RequestParam String tenant) {
+        RetData<String> result = null;
+
+        try {
+            NamedParameterJdbcTemplate namedParameterJdbcTemplate = commonDataSourceCache.getNamedParameterJdbcTemplate(tenant);
+
+            result = RetUtil.buildSuccessRet(namedParameterJdbcTemplate.getJdbcOperations().toString());
+        } catch (Exception e) {
+            logger.error("query namedParameterJdbcTemplate error", e);
             result = RetUtil.buildErrorRet(RetStatus.QUERY_ERROR);
         }
         return result;
