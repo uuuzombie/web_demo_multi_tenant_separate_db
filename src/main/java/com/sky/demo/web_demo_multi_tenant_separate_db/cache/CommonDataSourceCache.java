8616ec4b237aecbd3ce7927e56198fb2b55bd7e2
rg <uuuzombie@gmail.com>
2016-09-28 16:38:47 +0800

    mimus max connection of db

--- src/main/java/com/sky/demo/web_demo_multi_tenant_separate_db/cache/CommonDataSourceCache.java
+++ src/main/java/com/sky/demo/web_demo_multi_tenant_separate_db/cache/CommonDataSourceCache.java
@@ -177,7 +177,7 @@ public class CommonDataSourceCache {
         dataSource.setUsername(AppConfig.getItem("postgre.jdbc.username"));
         dataSource.setPassword(AppConfig.getItem("postgre.jdbc.password"));
         dataSource.setInitialSize(3);       //连接池启动时初始化连接数，默认0
-        dataSource.setMaxActive(100);       //连接池最大连接数
+        dataSource.setMaxActive(30);        //连接池最大连接数
         dataSource.setMinIdle(0);           //连接池最小空闲的连接数，低于此数会创建新连接
         dataSource.setMaxIdle(3);           //连接池最大空闲的连接数，超过的空闲链接将被释放，-1表示不限
         dataSource.setMaxWait(50000);       //最大等待时间，当没有可用链接时，等待释放的最大时间，超时则抛异常，-1表示不限
