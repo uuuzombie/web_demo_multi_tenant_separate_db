e247676329ee66b1108ad0aa363d71c7fc3ee56b
rg <uuuzombie@gmail.com>
2016-10-08 17:07:43 +0800

    update sql

--- depoly/depoly.sh
+++ depoly/depoly.sh
@@ -12,13 +12,14 @@ DB_NAME=pgtest
 
 #export PGPASSWORD=dbuser
 
+DATE=`date +%Y-%m-%d`
 
 ImportSQL()
 {
     for FILE in ${SQL_FILES}; do
         echo "==> import sql : ${FILE}"
         #psql -h ${DB_IP} -p ${DB_PORT} -U ${DB_USERNAME} -d ${DB_NAME} < ${FILE}
-        psql -f ${FILE} "host=${DB_IP} port=${DB_PORT} user=${DB_USERNAME} password=${DB_PASSWD}" | grep "ERROR"
+        psql -f ${FILE} "host=${DB_IP} port=${DB_PORT} user=${DB_USERNAME} password=${DB_PASSWD}" | grep "ERROR" | tee -a /tmp/$DATE.log
     done
 }
 
@@ -28,7 +29,7 @@ echo "====> start import sql"
 if [ -n "${INIT_SQL}" ]; then
     echo "==> import init sql"
     #psql -h ${DB_IP} -p ${DB_PORT} -U ${DB_USERNAME} -d ${DB_NAME} < ${FILE}
-    sudo -u postgres psql -f ${INIT_SQL}  | grep "ERROR"
+    sudo -u postgres psql -f ${INIT_SQL}  | grep "ERROR" | tee -a /tmp/$DATE.log
 fi
 
 
